389dfb4787e50261a97c66f89dc3157943ca1c64
Sangnd <sangnd@nexlesoft.com>
2022-07-11 15:52:32 +0700

    no message

--- app/src/main/java/com/example/ecommerce/Activity/ChiTietSanPham.kt
+++
@@ -1,5 +0,0 @@
-package com.example.ecommerce.Activity
-
-class ChiTietSanPham {
-
-}
--- app/src/main/java/com/example/ecommerce/Activity/MainActivity.kt
+++ app/src/main/java/com/example/ecommerce/Activity/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.ecommerce.Activity
 
-import android.app.ProgressDialog
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -20,14 +19,11 @@ class MainActivity : AppCompatActivity() {
     private var email: String? = null
     private var password: String? = null
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         email = "".also { password = it }
-        var pDialog: ProgressDialog = ProgressDialog(this)
-        pDialog.setMessage("Loading.......")
-        pDialog.setCanceledOnTouchOutside(false)
+
         button.setOnClickListener(View.OnClickListener {
             var email = edEmail.text.toString().trim()
             var password = edPassword.text.toString().trim()
@@ -35,58 +31,53 @@ class MainActivity : AppCompatActivity() {
                 val intent = Intent(this@MainActivity, HomeActivity::class.java)
                 startActivity(intent)
             } else {
-                if (checkEditText(edPassword) && isValidEmail(email)) {
-                    pDialog.show()
-                    val sr: StringRequest = object :
-                        StringRequest(Method.POST, Server.signin, Response.Listener { response ->
-                            if (response.equals("success")) {
-                                Log.d("sang", "hk")
-                                intent = Intent(this@MainActivity, HomeActivity::class.java)
-                                startActivity(intent)
-                            } else if (response.equals("failure")) {
-                                Toast.makeText(
-                                    this@MainActivity, "Mật khẩu bạn nhập không đúng",
-                                    Toast.LENGTH_LONG
-                                ).show()
-                            }
-                        }, Response.ErrorListener { error ->
+                val sr: StringRequest = object :
+                    StringRequest(Method.POST, Server.signin, Response.Listener { response ->
+                        if (response.equals("success")) {
+                            Log.d("sang", "hk")
+                            intent = Intent(this@MainActivity, HomeActivity::class.java)
+                            startActivity(intent)
+                        } else if (response.equals("failure")) {
                             Toast.makeText(
-                                applicationContext,
-                                error.toString().trim { it <= ' ' },
+                                this@MainActivity, "Mật khẩu bạn nhập không đúng",
                                 Toast.LENGTH_LONG
                             ).show()
-                        }) {
-                        @Throws(AuthFailureError::class)
-                        override fun getParams(): Map<String, String>? {
-                            val data: MutableMap<String, String> = HashMap()
-                            data["email"] = email
-                            data["password"] = password
-                            return data
                         }
+                    }, Response.ErrorListener { error ->
+                        Toast.makeText(
+                            applicationContext,
+                            error.toString().trim { it <= ' ' },
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }) {
+                    @Throws(AuthFailureError::class)
+                    override fun getParams(): Map<String, String>? {
+                        val data: MutableMap<String, String> = HashMap()
+                        data["email"] = email
+                        data["password"] = password
+                        return data
                     }
-                    var requestQueue = Volley.newRequestQueue(applicationContext)
-                    requestQueue.add<String>(sr)
                 }
+                var requestQueue = Volley.newRequestQueue(applicationContext)
+                requestQueue.add<String>(sr)
             }
         })
+
     }
 
-
-    private fun checkEditText(editText: EditText): Boolean {
-        if (editText.text.toString().trim { it <= ' ' }.length > 0) return true else {
-            editText.error = "Vui lòng nhập dữ liệu!"
-        }
-        return false
-    }
-
-    //
-    private fun isValidEmail(target: String): Boolean {
-        if (target.toRegex().matches("[a-zA-Z0-9._-]+@[a-z]+.[a-z]+")) return true else {
-            edEmail.setError("Email sai định dạng!")
-        }
-        return false
-    }
-
+    //    private fun checkEditText(editText: EditText): Boolean {
+//        if (editText.text.toString().trim { it <= ' ' }.length > 0) return true else {
+//            editText.error = "Vui lòng nhập dữ liệu!"
+//        }
+//        return false
+//    }
+//
+//    private fun isValidEmail(target: String): Boolean {
+//        if (target.toRegex().matches("[a-zA-Z0-9._-]+@[a-z]+.[a-z]+")) return true else {
+//            edEmail.setError("Email sai định dạng!")
+//        }
+//        return false
+//    }
     fun register(view: View) {
         intent = Intent(this@MainActivity, RegisterActivity::class.java)
         startActivity(intent)
