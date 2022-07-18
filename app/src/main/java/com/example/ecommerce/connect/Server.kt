5169f301bf0b815b63b304ec34f000204006cf52
sanghk38 <79837195+sanghk38@users.noreply.github.com>
2022-07-18 17:47:26 +0700

    TMDD - 0001- commit one

--- app/src/main/java/com/example/ecommerce/connect/Server.kt
+++ app/src/main/java/com/example/ecommerce/connect/Server.kt
@@ -1,8 +1,8 @@
 package com.example.ecommerce.connect
 
  object Server {
-    private var url = "http://192.168.0.104"
-    var signup = url + "/login/register.php"
-    var signin = url + "/login/login.php"
+    private var url = "http://192.168.50.34"
+    var signup = url + "/mobilephp/sign_up.php"
+    var signin = url + "/mobilephp/sign_in.php"
 
 }
